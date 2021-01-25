f97da67276aad55425678a2d700dfea94bde6372
Ulrich Braunschweig <u.braunschweig@utoronto.ca>
2021-01-25 18:11:29 -0500

    Delete GOplotTools_runfile.R

--- GOplotTools_runfile.R
+++
@@ -1,8 +0,0 @@
-source("GOplotTools.R")
-
-plotFuncAssDots(file="input/FuncAssociate_results.tsv",
-                outName="output/FuncAssociate_plotFuncAssDots.pdf",
-                inputGenes="input/Input_genesWithChangingExons.txt",
-                attrEntList="input/FuncAssociate_attrEntList.xls",
-                main="Genes enriched in input over background",
-                wid=9, hei=4.5)
